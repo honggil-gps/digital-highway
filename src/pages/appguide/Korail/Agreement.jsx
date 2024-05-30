589534b91b16124d8a7a7fd1ae3a911b8eaf8c1b
yun9699 <cha23215@daum.net>
2024-05-30 13:25:25 +0900

    krail passSMS 인적정보 사항 기능 수정_이윤창

--- src/pages/appguide/Korail/Agreement.jsx
+++ src/pages/appguide/Korail/Agreement.jsx
@@ -1,3 +1,113 @@
+// import { useState, useCallback } from "react";
+// import { useNavigate } from "react-router-dom";
+// import PopupMenubar from "./PopupMenubar";
+// import PortalDrawer from "./PortalDrawer";
+// import "./Agreement.css";
+
+// const Agreement = () => {
+//   const navigate = useNavigate();
+//   const [isPopupMenubarOpen, setPopupMenubarOpen] = useState(false);
+
+//   const onAgreementFooterClick = useCallback(() => {
+//     navigate("/maincontents/pass-05");
+//   }, [navigate]);
+
+//   const openPopupMenubar = useCallback(() => {
+//     setPopupMenubarOpen(true);
+//   }, []);
+
+//   const closePopupMenubar = useCallback(() => {
+//     setPopupMenubarOpen(false);
+//   }, []);
+
+//   return (
+//     <>
+//       <div className="krail-agreement-04">
+//         <button className="krail-agreementfooter" onClick={onAgreementFooterClick}>
+//           <b className="krail-b25">다음</b>
+//         </button>
+//         <main className="krail-agreementmain">
+//           <div className="krail-agreementmain3">
+//             <div className="krail-agreementmain3c">
+//               <input className="krail-rectangle-input" type="checkbox" />
+//               <div className="krail-frame">
+//                 <b className="krail-b26">
+//                   이벤트/혜택/마케팅을 위한 정보 활용에 동의합니다.(선택)
+//                 </b>
+//                 <b className="krail-b27">
+//                   *선택정보에 동의하지 않아도 서비스 이용제한은 없습니다.
+//                 </b>
+//               </div>
+//             </div>
+//             <div className="krail-agreementmain3b">
+//               <input className="krail-rectangle-input" type="checkbox" />
+//               <div className="krail-frame1">
+//                 <b className="krail-b28">
+//                   선택 개인정보 수집 및 이용에 동의합니다.(선택)
+//                 </b>
+//                 <b className="krail-b29">
+//                   *선택정보에 동의하지 않아도 서비스 이용제한은 없습니다.
+//                 </b>
+//               </div>
+//             </div>
+//             <div className="krail-agreementmain3a">
+//               <h2 className="krail-h2">개인정보 수집 및 이용에 동의합니다.(필수)</h2>
+//               <input className="krail-rectangle-input" type="checkbox" />
+//             </div>
+//             <div className="krail-agreementmain3ttitle">
+//               <div className="krail-rectangle-parent">
+//                 <div className="krail-frame-child" />
+//                 <b className="krail-b30">약관보기</b>
+//               </div>
+//               <h1 className="krail-h1">
+//                 <ol className="krail-ol">
+//                   <li>개인정보 수집 및 이용에 대한 안내</li>
+//                 </ol>
+//               </h1>
+//             </div>
+//           </div>
+//           <div className="krail-agreementmain2">
+//             <div className="krail-agreementmain2line" />
+//             <div className="krail-agreementmain2check">
+//               <b className="krail-b31">회원약관 동의(필수)</b>
+//               <input className="krail-rectangle-input" type="checkbox" />
+//             </div>
+//             <div className="krail-wrapper">
+//               <b className="krail-b32">약관보기</b>
+//             </div>
+//             <h1 className="krail-h11">
+//               <ol className="krail-ol">
+//                 <li>코레일톡 회원약관</li>
+//               </ol>
+//             </h1>
+//           </div>
+//           <div className="krail-agreementmain1">
+//             <div className="krail-agreementmain1line" />
+//             <b className="krail-b31">회원약관 전체 동의(필수)</b>
+//             <input className="krail-rectangle-input" type="checkbox" />
+//           </div>
+//         </main>
+//         <header className="krail-agreementheader">
+//           <b className="krail-b34">회원가입</b>
+//           <button className="krail-agreementheaderbutton" onClick={openPopupMenubar}>
+//             <div className="krail-agreementheaderbuttonline3" />
+//             <div className="krail-agreementheaderbuttonline2" />
+//             <div className="krail-agreementheaderbuttonline1" />
+//           </button>
+//         </header>
+//       </div>
+//       {isPopupMenubarOpen && (
+//         <PortalDrawer placement="Right" onOutsideClick={closePopupMenubar}>
+//           <PopupMenubar onClose={closePopupMenubar} />
+//         </PortalDrawer>
+//       )}
+//     </>
+//   );
+// };
+
+// export default Agreement;
+
+
 import { useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import PopupMenubar from "./PopupMenubar";
@@ -83,7 +193,7 @@ const Agreement = () => {
           </div>
           <div className="krail-agreementmain1">
             <div className="krail-agreementmain1line" />
-            <b className="krail-b31">회원약관 동의(필수)</b>
+            <b className="krail-b31">회원약관 전체 동의(필수)</b>
             <input className="krail-rectangle-input" type="checkbox" />
           </div>
         </main>
--- src/pages/appguide/Korail/PassSms.jsx
+++ src/pages/appguide/Korail/PassSms.jsx
@@ -1,3 +1,152 @@
+// import { useState, useCallback } from "react";
+// import PopupCheckMember from "./PopupCheckMember";
+// import PortalPopup from "./PortalPopup";
+// import PopupMenubar from "./PopupMenubar";
+// import { useNavigate } from "react-router-dom";
+// import "./PassSms.css";
+
+// const PassSms = () => {
+//   const [isPopupCheckMemberOpen, setPopupCheckMemberOpen] = useState(false);
+//   const [isPopupMenubarOpen, setPopupMenubarOpen] = useState(false);
+//   const navigate = useNavigate();
+
+//   const openPopupCheckMember = useCallback(() => {
+//     setPopupCheckMemberOpen(true);
+//   }, []);
+
+//   const closePopupCheckMember = useCallback(() => {
+//     setPopupCheckMemberOpen(false);
+//   }, []);
+
+//   const onSmsMainCancelButtonClick = useCallback(() => {
+//     navigate("/maincontents/pass-05");
+//   }, [navigate]);
+
+//   const openPopupMenubar = useCallback(() => {
+//     setPopupMenubarOpen(true);
+//   }, []);
+
+//   const closePopupMenubar = useCallback(() => {
+//     setPopupMenubarOpen(false);
+//   }, []);
+
+
+//   function moveFocus(current, nextId) {
+//     current.value = current.value.replace(/[^0-9]/g, '');
+//     if (current.value.length >= 4 && nextId) {
+//       document.getElementById(nextId).focus();
+//     }
+//   }
+
+//   function validateNumberInput(object) {
+//     object.value = object.value.replace(/[^0-9]/g, '');
+//   }
+
+//   return (
+//     <>
+//       <div className="krail-passsms-06">
+//         <footer className="krail-passsmsfooter">
+//           <img
+//             className="krail-passfooterimage-icon1"
+//             alt=""
+//             src="/appguide/Korail/passfooterimage@2x.png"
+//           />
+//           <b className="krail-b209">통신 암호화 처리중</b>
+//         </footer>
+//         <main className="krail-passsmsheadermain">
+//           <div className="krail-smsmainbuttonset">
+//             <button
+//               className="krail-smsmainconfirmbutton"
+//               onClick={openPopupCheckMember}
+//             >
+//               <b className="krail-b210">확인</b>
+//             </button>
+//             <button
+//               className="krail-smsmaincancelbutton"
+//               onClick={onSmsMainCancelButtonClick}
+//             >
+//               <b className="krail-b211">취소</b>
+//             </button>
+//           </div>
+//           <div className="krail-smsmainremembercheck">
+//             <div className="krail-smsmainremembercheckbox" />
+//             <b className="krail-b212">인증정보(이름/휴대폰번호) 기억하기</b>
+//           </div>
+//           <div className="krail-smsmaintellnumber">
+//             <input className="krail-smsmaintellnumberinput" type="tel" maxlength="11" onInput={(e) => validateNumberInput(e.target)} />
+//             <b className="krail-b213">전화번호</b>
+//           </div>
+//           <div className="krail-smsmainresidentregistration">
+//             <b className="krail-b213">생년월일/성별</b>
+//             <div className="krail-smsmainresidentregistrationinp">
+//               <div className="krail-residentregistrationellipseset">
+//                 <div className="krail-residentregistrationellipse1" />
+//                 <div className="krail-residentregistrationellipse1" />
+//                 <div className="krail-residentregistrationellipse1" />
+//                 <div className="krail-residentregistrationellipse1" />
+//                 <div className="krail-residentregistrationellipse1" />
+//                 <div className="krail-residentregistrationellipse1" />
+//               </div>
+//               <input className="krail-genderdateinput" type="text" 
+//               maxlength="1" onInput={(e) => {validateNumberInput(e.target); moveFocus(e.target, '')}} id="input2"
+//               />
+//               <div className="krail-smsmainresidentregistrationlin" />
+//               <input className="krail-birthdateinput1" type="text"  
+//               maxlength="6" onInput={(e) => {validateNumberInput(e.target); moveFocus(e.target, 'input2')}} id="input1"
+//               />
+//             </div>
+//           </div>
+//           <div className="krail-smsmainname">
+//             <input className="krail-smsmainnameinput" type="text" />
+//             <b className="krail-b215">이름</b>
+//           </div>
+//           <img
+//             className="krail-passmainttitleimage-icon1"
+//             alt=""
+//             src="/appguide/Korail/passmainttitleimage@2x.png"
+//           />
+//         </main>
+//         <header className="krail-passsmsheader">
+//           <b className="krail-sms3">휴대폰 본인 확인 - 문자SMS</b>
+//           <div className="krail-headerbutton1">
+//             <div className="krail-agreementheaderbuttonline32" />
+//             <div className="krail-agreementheaderbuttonline22" />
+//             <div className="krail-agreementheaderbuttonline12" />
+//           </div>
+//         </header>
+//         <header className="krail-passsmsheader">
+//           <b className="krail-sms3">휴대폰 본인 확인 - 문자SMS</b>
+//           <button className="krail-headerbutton2" onClick={openPopupMenubar}>
+//             <div className="krail-agreementheaderbuttonline32" />
+//             <div className="krail-agreementheaderbuttonline22" />
+//             <div className="krail-agreementheaderbuttonline12" />
+//           </button>
+//         </header>
+//       </div>
+//       {isPopupCheckMemberOpen && (
+//         <PortalPopup
+//           overlayColor="rgba(113, 113, 113, 0.3)"
+//           placement="Centered"
+//         >
+//           <PopupCheckMember onClose={closePopupCheckMember} />
+//         </PortalPopup>
+//       )}
+//       {isPopupMenubarOpen && (
+//         <PortalPopup
+//           overlayColor="rgba(113, 113, 113, 0.3)"
+//           placement="Centered"
+//           onOutsideClick={closePopupMenubar}
+//         >
+//           <PopupMenubar onClose={closePopupMenubar} />
+//         </PortalPopup>
+//       )}
+//     </>
+//   );
+// };
+
+// export default PassSms;
+
+
 import { useState, useCallback } from "react";
 import PopupCheckMember from "./PopupCheckMember";
 import PortalPopup from "./PortalPopup";
@@ -8,11 +157,19 @@ import "./PassSms.css";
 const PassSms = () => {
   const [isPopupCheckMemberOpen, setPopupCheckMemberOpen] = useState(false);
   const [isPopupMenubarOpen, setPopupMenubarOpen] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [gender, setGender] = useState("");
+  const [name, setName] = useState("");
   const navigate = useNavigate();
 
   const openPopupCheckMember = useCallback(() => {
-    setPopupCheckMemberOpen(true);
-  }, []);
+    if (phoneNumber && birthDate && gender && name) {
+      setPopupCheckMemberOpen(true);
+    } else {
+      alert("빈칸을 모두 채워주세요!");
+    }
+  }, [phoneNumber, birthDate, gender, name]);
 
   const closePopupCheckMember = useCallback(() => {
     setPopupCheckMemberOpen(false);
@@ -29,6 +186,17 @@ const PassSms = () => {
   const closePopupMenubar = useCallback(() => {
     setPopupMenubarOpen(false);
   }, []);
+
+  function moveFocus(current, nextId) {
+    current.value = current.value.replace(/[^0-9]/g, '');
+    if (current.value.length >= 6 && nextId) {
+      document.getElementById(nextId).focus();
+    }
+  }
+
+  function validateNumberInput(object) {
+    object.value = object.value.replace(/[^0-9]/g, '');
+  }
 
   return (
     <>
@@ -61,7 +229,14 @@ const PassSms = () => {
             <b className="krail-b212">인증정보(이름/휴대폰번호) 기억하기</b>
           </div>
           <div className="krail-smsmaintellnumber">
-            <input className="krail-smsmaintellnumberinput" type="tel" />
+            <input 
+              className="krail-smsmaintellnumberinput" 
+              type="tel" 
+              maxLength="11" 
+              onInput={(e) => validateNumberInput(e.target)} 
+              value={phoneNumber} 
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
             <b className="krail-b213">전화번호</b>
           </div>
           <div className="krail-smsmainresidentregistration">
@@ -75,13 +250,34 @@ const PassSms = () => {
                 <div className="krail-residentregistrationellipse1" />
                 <div className="krail-residentregistrationellipse1" />
               </div>
-              <input className="krail-genderdateinput" type="number" />
+              <input 
+                className="krail-genderdateinput" 
+                type="text" 
+                maxLength="1" 
+                onInput={(e) => {validateNumberInput(e.target); moveFocus(e.target, '')}} 
+                id="input2"
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+              />
               <div className="krail-smsmainresidentregistrationlin" />
-              <input className="krail-birthdateinput1" type="number" />
+              <input 
+                className="krail-birthdateinput1" 
+                type="text" 
+                maxLength="6" 
+                onInput={(e) => {validateNumberInput(e.target); moveFocus(e.target, 'input2')}} 
+                id="input1"
+                value={birthDate}
+                onChange={(e) => setBirthDate(e.target.value)}
+              />
             </div>
           </div>
           <div className="krail-smsmainname">
-            <input className="krail-smsmainnameinput" type="text" />
+            <input 
+              className="krail-smsmainnameinput" 
+              type="text" 
+              value={name} 
+              onChange={(e) => setName(e.target.value)}
+            />
             <b className="krail-b215">이름</b>
           </div>
           <img
