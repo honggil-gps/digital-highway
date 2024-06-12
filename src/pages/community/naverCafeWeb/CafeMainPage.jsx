9f3b38726d446d872142303bc78b0310dd84d048
honggil-gps <ghdrlf718@naver.com>
2024-06-13 08:25:43 +0900

    PaginationNumberUpdateHG

--- src/pages/community/naverCafeWeb/CafeMainPage.jsx
+++ src/pages/community/naverCafeWeb/CafeMainPage.jsx
@@ -1,78 +1,3 @@
-// import { useState, useEffect, useCallback } from "react";
-// import { useNavigate } from "react-router-dom";
-// import axios from "axios"
-// import PostViewHead from "../../../components/community/naverCafeWeb/PostViewHead";
-// import PostTable from "../../../components/community/naverCafeWeb/PostTable";
-// import PageNumberFrame from "../../../components/community/naverCafeWeb/PageNumberFrame";
-// import CafeSearchbarFrame from "../../../components/community/naverCafeWeb/CafeSearchbarFrame";
-// import NaverCafeSidebar from "../../../components/community/naverCafeWeb/NaverCafeSidebar1";
-// import "./CafeMainPage.css";
-
-// const CafeMainPage = () => {
-//   const navigate = useNavigate();
-//   const [posts, setPosts] = useState([]);
-//   const [searchQuery, setSearchQuery] = useState("");
-
-//   useEffect(()=>{
-//     fetchPosts();
-//   },[searchQuery])
-
-//   const fetchPosts = useCallback(async () => {
-//     try {
-//       const response = await axios.get('http://localhost:4000/community/',{withCredentials:true});
-//       setPosts(response.data);
-//     } catch (error) {
-//       console.error('Error fetching posts:', error);
-//     }
-//   }, []);
-
-//   const onButtonClick = useCallback(() => {
-//     navigate("/community/naverCafeWeb/");
-//   }, [navigate]);
-
-//   const onButton1Click = useCallback(() => {
-//     navigate("/community/naverCafeWeb/cafehotpostpage");
-//   }, [navigate]);
-
-//   const onTextClick = useCallback((id) => {
-//     navigate("/community/naverCafeWeb/cafepostview",{state:{id}});
-//   }, [navigate]);
-
-//   const onSearchbarButtonClick = useCallback(() => {
-//     navigate("/community/naverCafeWeb/cafesearchafterpage", {state:{searchQuery, posts}});
-//   }, [navigate, searchQuery, posts]);
-
-//   const onCafeWritingButtonClick = useCallback(() => {
-//     navigate("/community/naverCafeWeb/cafewritingpost");
-//   }, [navigate]);
-
-//   return (
-//     <div className="ncafe-cafemainpage">
-//       <img
-//         className="ncafe-cafemainpage-child"
-//         alt=""
-//         src="/community/naverCafeWeb/bannerimg.png"
-//       />
-//       <PostViewHead
-//         propColor="#000"
-//         onButtonClick={onButtonClick}
-//         onButton1Click={onButton1Click}
-//       />
-//       <PostTable
-//         posts={posts}
-//         onTextClick={onTextClick}
-//       />
-//       <PageNumberFrame />
-//       <CafeSearchbarFrame 
-//       searchQuery={searchQuery}
-//       setSearchQuery={setSearchQuery}
-//       onSearchbarButtonClick={onSearchbarButtonClick} />
-//       <NaverCafeSidebar onCafeWritingButtonClick={onCafeWritingButtonClick} />
-//     </div>
-//   );
-// };
-
-// export default CafeMainPage;
 import React, { useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -88,7 +13,7 @@ const CafeMainPage = () => {
   const [posts, setPosts] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const postsPerPage = 3;
+  const postsPerPage = 10;
 
   useEffect(() => {
     fetchPosts();
